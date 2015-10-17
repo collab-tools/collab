28026788edeb9c84a89a37b098090278cda6de71
seowyanyi <seow.yanyi@gmail.com>
2015-10-17 14:35:52 +0800

    style header bar

--- assets/app/js/components/Header.jsx
+++ assets/app/js/components/Header.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Sticky from 'react-sticky'
 import LeftPanel from '../components/LeftPanel.jsx';
 import $ from 'jquery';
 
@@ -34,15 +33,17 @@ class Header extends Component {
 
     render() {
        return (
-               <Sticky>
-                    <header className='app-header'>
-                      <LeftPanel visibility={this.state.panel_visible} showLeft={e => this.showLeft()} 
-                        hideLeft={e => this.hideLeft()} />
-                      <button onClick={e => this.showLeft()}>Show Left Menu!</button>
-                      <button className='btn btn-default logout' onClick={e => this.logOut()}>Log Out</button>
-                      <h1>NUS Collab </h1>
-                    </header>
-               </Sticky>
+               <div className='app-header'>
+                    <nav>
+                        <ul>
+                            <li><span onClick={e => this.showLeft()}>Projects</span></li>
+                            <li><a href="#">NUS Collab</a></li>
+                            <li><a href="#"> Log Out </a></li>
+                        </ul>                
+                    </nav>
+                        <LeftPanel visibility={this.state.panel_visible} showLeft={e => this.showLeft()} 
+                            hideLeft={e => this.hideLeft()} />
+               </div>
            );
     }
 }
