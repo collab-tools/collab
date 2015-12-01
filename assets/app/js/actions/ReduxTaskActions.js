758ce50e02ffce208019bcfc37d49aeab998ed6a
seowyanyi <seow.yanyi@gmail.com>
2015-12-02 02:21:34 +0800

    temporarily mock notifications

--- assets/app/js/actions/ReduxTaskActions.js
+++ assets/app/js/actions/ReduxTaskActions.js
@@ -48,6 +48,23 @@ export function dismissProjectAlert() {
     }
 }
 
+const notifState = [
+    {
+        id: 'notif-1',
+        text: 'Cristina invited you to the project CS3201',
+        time: new Date().toISOString(), 
+        link: 'http://www.nus.edu.sg/',
+        read: false
+    },
+    {
+        id: 'notif-2',            
+        text: 'Ken uploaded a file in CG3002',
+        time: new Date().toISOString(), 
+        link: 'http://www.nus.edu.sg/',            
+        read: false
+    }
+];
+
 export function initializeApp() {
     return function(dispatch) {
         dispatch(initUsers([{
