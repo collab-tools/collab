779e2139e62216cf3a151b7392db114039e03db0
seowyanyi <seow.yanyi@gmail.com>
2015-12-14 18:07:27 +0800

    use material UI for milestone list

--- assets/app/js/components/CompletedRow.jsx
+++ assets/app/js/components/CompletedRow.jsx
@@ -1,51 +1,31 @@
 import React, { Component } from 'react'
-
-class CompletedTask extends Component {
-    render() {
-        return (<div><span>{this.props.taskName}</span></div>);
-    }
-}
+import ListItem from 'material-ui/lib/lists/list-item'
+import List from 'material-ui/lib/lists/list'
 
 class CompletedRow extends Component {
     constructor(props, context) {
         super(props, context); 
-        this.state = {
-            toShow: false
-        };
     }
 
-    handleChange() {
-        this.setState({
-            toShow: !this.state.toShow
-        });
-    }
     render() {
-        if (this.props.completedTasks.length === 0) {
-            return (
-                <div className='completed-row'></div>
-            );
-        }
 
-        if (this.state.toShow) {
-            let rows = this.props.completedTasks.map(task =>                    
-                <span className='completed-item' key={_.uniqueId('completed')}>{task.content}</span>
-            );
-
-            return (
-                <div className='completed-row'>
-                    <span className="completed-text" onClick={e => this.handleChange()}> Completed </span>
-                    {rows}
-                </div>
-                );
-        }
+        let rows = this.props.completedTasks.map(task =>
+            <ListItem
+                key={_.uniqueId('completed')}
+                primaryText={task.content}
+            />
+        )
 
         return (
-            <div className='completed-row'>
-                <span className="completed-text" onClick={e => this.handleChange()}>
-                    {this.props.completedTasks.length} completed
-                </span>
-            </div>);
+            <List>
+                <ListItem
+                    key={_.uniqueId('completedHeader')}
+                    primaryText={this.props.completedTasks.length + ' completed'}
+                    nestedItems={rows}
+                />
+            </List>
+        )
     }
 }
 
-export default CompletedRow;
+export default CompletedRow
--- assets/app/js/components/MilestoneView.jsx
+++ assets/app/js/components/MilestoneView.jsx
@@ -3,23 +3,8 @@ import _ from 'lodash'
 import $ from 'jquery'
 import MilestoneRow from './MilestoneRow.jsx'
 import CompletedRow from './CompletedRow.jsx'
-
-class TaskRow extends Component {
-    render() {
-        let dateStr = "";
-        if (this.props.task.deadline !== null) {
-            dateStr = this.props.task.deadline;
-        }
-        return (
-            <div className='taskrow'>
-                <i className='fa fa-circle-thin' onClick={this.props.onClickDone}> </i>    
-                <span className='task-content'>{this.props.task.content}</span>
-                <span>{dateStr}</span>        
-                <i className='fa fa-close' onClick={this.props.onClickDelete}></i>
-            </div>
-        );
-    }
-}
+import Trash from './../icons/Trash.jsx'
+import {List, ListDivider, ListItem, Checkbox, FlatButton, IconButton } from 'material-ui'
 
 class MilestoneView extends Component {
     constructor(props, context) {
@@ -59,39 +44,50 @@ class MilestoneView extends Component {
         let rows = [];
 
         this.props.milestones.forEach(milestone => {
-            rows.push(<MilestoneRow
-                milestone={milestone.content}
-                key={milestone.id}
-                onAddTask={this.addTask.bind(this, milestone.id)}
-                />);
-
+            rows.push(<h3 key={milestone.id}>{milestone.content}</h3>)
+            let tasks = []
             this.props.tasks.forEach(task => {
                 // Only show non-completed tasks and non-dirtied tasks
-                if (task.completed_on === null && 
-                    task.dirty !== true && 
+                if (task.completed_on === null &&
+                    task.dirty !== true &&
                     task.milestone_id === milestone.id) {
-                    rows.push(<TaskRow
-                        task={task}
-                        key={task.id}
-                        onClickDone={this.markDone.bind(this, task.id)}
-                        onClickDelete={this.deleteTask.bind(this, task.id)}
-                        />);
+                    tasks.push(
+                        <ListItem
+                            key={_.uniqueId('task')}
+                            primaryText={task.content}
+                            leftCheckbox={
+                                <Checkbox
+                                    onCheck={this.markDone.bind(this, task.id)}
+                                />
+                            }
+                            rightIconButton={
+                                <IconButton onClick={this.deleteTask.bind(this, task.id)}>
+                                    <Trash/>
+                                </IconButton>
+                            }
+                        />)
                 }
-            });
+            }) // task.forEach end
 
-            rows.push(<CompletedRow
-                key={_.uniqueId('completed')}
-                completedTasks={this.getCompletedTasks(milestone.id)}
-                />);
+            rows.push(<List key={_.uniqueId()}>{tasks}</List>)
 
+            let completedTasks = this.getCompletedTasks(milestone.id)
+            if (completedTasks.length > 0) {
+                rows.push(<CompletedRow
+                    key={_.uniqueId('completed')}
+                    completedTasks={completedTasks}
+                />)
+            }
+
+            rows.push(<ListDivider key={_.uniqueId('divider')}/>)
         });
 
         return (
             <div className='milestone-view'>
                 <div className='task-items'>
                     {rows}
-                </div>          
-            </div>    
+                </div>
+            </div>
         );
     }
 }
