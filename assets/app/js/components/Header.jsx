d2287eab2e4e9115bc0a0979cecd8ce0c7995266
seowyanyi <seow.yanyi@gmail.com>
2016-02-04 01:39:56 +0800

    display picture on header bar

--- assets/app/js/components/Header.jsx
+++ assets/app/js/components/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router'
 import $ from 'jquery'
 import { Navbar, Nav, NavItem, Badge } from 'react-bootstrap'
 import {logout} from '../utils/auth.js'
+import Avatar from 'material-ui/lib/avatar';
 
 class Header extends Component {
 
@@ -17,7 +18,7 @@ class Header extends Component {
             <div>
             <Navbar className='nav-bar header-color' fluid={true} fixedTop={true}>
                 <Navbar.Header>
-                    <Navbar.Brand className="nav-brand">
+                    <Navbar.Brand className="nav-brand padded-nav">
                         <a href="/app"><span className="header-text">Collab</span></a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
@@ -26,7 +27,7 @@ class Header extends Component {
                     <Nav className="right-nav" pullRight>
                         <LinkContainer to='/app/notifications' >
                             <NavItem
-                                className='nav-link'
+                                className='nav-link padded-nav'
                                 eventKey={2}>
                                 <span className="header-text">Notifs </span>{badge}
                             </NavItem>
@@ -34,6 +35,7 @@ class Header extends Component {
                         <NavItem
                             className='nav-link'
                             eventKey={3}>
+                            <Avatar src={localStorage.getItem('display_image')} />
                             <span className="header-text">{this.props.displayName} </span>
                         </NavItem>
                     </Nav>
