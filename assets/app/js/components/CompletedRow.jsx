a2bbbc3c60a1788cb620fd8b1f035fa63ddb24e7
seowyanyi <seow.yanyi@gmail.com>
2016-03-10 23:53:30 +0800

    show completed tasks

--- assets/app/js/components/CompletedRow.jsx
+++ assets/app/js/components/CompletedRow.jsx
@@ -1,29 +1,57 @@
 import React, { Component } from 'react'
 
-class CompletedRow extends Component {
+class CompletedItem extends Component {
     constructor(props, context) {
         super(props, context); 
     }
 
     render() {
-        return (<div></div>)
-        //
-        //let rows = this.props.completedTasks.map(task =>
-        //    <ListItem
-        //        key={_.uniqueId('completed')}
-        //        primaryText={task.content}
-        //    />
-        //)
-        //
-        //return (
-        //    <List>
-        //        <ListItem
-        //            key={_.uniqueId('completedHeader')}
-        //            primaryText={this.props.completedTasks.length + ' completed'}
-        //            nestedItems={rows}
-        //        />
-        //    </List>
-        //)
+        return (
+            <li>{this.props.text}</li>
+        )
+    }
+}
+
+class CompletedRow extends Component {
+    constructor(props, context) {
+        super(props, context); 
+        this.state = {
+            hidden: true
+        }
+    }
+
+    toggle() {
+        this.setState({
+            hidden: !this.state.hidden
+        })  
+    }
+
+    render() {       
+        let rows = this.props.completedTasks.map(task =>
+           <CompletedItem
+               key={_.uniqueId('completed')}
+               text={task.content}
+           />
+        )
+
+        let list = null
+
+        if (!this.state.hidden) {
+            list = (
+                <ul>
+                    {rows}
+                </ul>
+            )
+        }
+        
+        return (
+            <div className="completed-task-list">
+                <div className="completed-text" onClick={this.toggle.bind(this)}>
+                    {this.props.completedTasks.length + ' completed'}
+                </div>
+                {list}
+            </div>
+        )
     }
 }
 
