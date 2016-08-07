9f4687876fd2da6060731517d85418a52f416273
Yeap Hooi Tong <hooitongx@gmail.com>
2016-08-07 23:47:38 +0800

    Changed dev workflow to be cross-platform friendly

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,25 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './assets/app/js/index.jsx'
-    },
-    output: {
-        path: __dirname + '/assets',
-        filename: '[name]-bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test:/\.css$/,
-                loader: 'style-loader!css-loader'
-            }
-        ]
-    }
+  entry: {
+    app: './assets/app/js/index.jsx'
+  },
+  output: {
+    path: __dirname + '/assets',
+    filename: '[name]-bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+  plugins: [new webpack.OldWatchingPlugin()]
 };
