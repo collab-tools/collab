45b0e641ec93edfa6e39e80a4a87deeb886d59d0
Zhang Ji <zhangji951027@gmail.com>
2017-01-10 01:52:14 +0800

    add enzyme & config

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,31 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './assets/app/js/index.jsx'
-    },
-    output: {
-        path: __dirname + '/assets',
-        filename: '[name]-bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test:/\.css$/,
-                loader: 'style-loader!css-loader'
-            }
-        ]
-    }
+  entry: {
+    app: './assets/app/js/index.jsx'
+  },
+  output: {
+    path: __dirname + '/assets',
+    filename: '[name]-bundle.js'
+  },
+  externals: {
+    'cheerio': 'window',
+    'react-addons-test-utils': true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test:/\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
 };
