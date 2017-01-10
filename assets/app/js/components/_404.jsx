f8741f22563e24a4a77f7c3dd9dbae9b4fe784ef
Zhang Ji <zhangji951027@gmail.com>
2017-01-10 16:55:32 +0800

    add first frontEnd test cases

--- assets/app/js/components/_404.jsx
+++ assets/app/js/components/_404.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class _404 extends Component {
     render() {
-        return (<div><h1>404</h1></div>);
+        return <div><h1>404</h1></div>;
     }
 }
 
