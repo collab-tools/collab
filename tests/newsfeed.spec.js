98f2235b043a03ce0ad0fb38c547f80dd25bfcf3
Benjamin <benjamin.heng@thecarousell.com>
2017-01-24 00:06:06 +0800

    Add initial test configs
    - Also add a basic eslint config

---
+++ tests/newsfeed.spec.js
@@ -0,0 +1,30 @@
+/* global sinon, expect, beforeEach, afterEach, it, describe */
+import newsfeed from '../server/controller/newsfeedController';
+import storage from '../server/data/storage';
+
+describe('Newsfeed', () => {
+  it('should save newsfeed post to disk and broadcast to project', (done) => {
+    const storageStub = sinon.stub(storage, 'saveNewsfeed');
+    const data = {ref_type: 'branch', ref: 'helloworld', user_id: 'NysSbasYe'}
+    const template = templates.GITHUB_CREATE
+    const projectId = '4yGslGste'
+    const newsfeedPost = {
+      id: 'newsfeed1',
+      data: JSON.stringify(data),
+      template: template,
+      project_id: projectId,
+      updated_at: '2016-03-27 06:22:21',
+      created_at: '2016-03-27 06:22:21'
+    }
+    storageStub.withArgs(JSON.stringify(data), template, projectId)
+      .returns(Promise.resolve(newsfeedPost));
+
+    const socketMock = sinon.mock(socket);
+    socketMock.expects('sendMessageToProject').once().withExactArgs(projectId, 'newsfeed_post', newsfeedPost)
+
+    newsfeed.updateNewsfeed(data, template, projectId, constants.GITHUB).then(function(res) {
+      expect(res).to.deep.equal(newsfeedPost)
+      done()
+    })
+  });
+});
---
+++ tests/setup.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+global.chai = require('chai');
+global.sinon = require('sinon');
+global.expect = require('chai').expect;
+
+global.chai.should();
