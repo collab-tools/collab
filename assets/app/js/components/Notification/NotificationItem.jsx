a78bd718f2b55102719c10b2523548df8eb38b55
Zhang Ji <zhangji951027@gmail.com>
2017-02-10 22:50:17 +0800

    resolve eslint warning on Notification components

--- assets/app/js/components/Notification/NotificationItem.jsx
+++ assets/app/js/components/Notification/NotificationItem.jsx
@@ -1,61 +1,61 @@
-import React, { Component, PropTypes } from 'react'
-import {Button} from 'react-bootstrap'
+import React, { Component, PropTypes } from 'react';
+import { Button } from 'react-bootstrap';
 
 import UserAvatar from '../UserAvatar.jsx';
-import {acceptProject, declineProject}  from '../../actions/ReduxTaskActions'
+import { acceptProject, declineProject } from '../../actions/ReduxTaskActions';
 
 class NotificationItem extends Component {
-    acceptProject() {
-        let notifId = this.props.id
-        let projectId = this.props.meta.project_id
-        this.props.dispatch(acceptProject(projectId, notifId))
+  acceptProject() {
+    const notifId = this.props.id;
+    const projectId = this.props.meta.project_id;
+    this.props.dispatch(acceptProject(projectId, notifId));
+  }
+
+  declineProject() {
+    const notifId = this.props.id;
+    const projectId = this.props.meta.project_id;
+    this.props.dispatch(declineProject(projectId, notifId));
+  }
+
+  render() {
+    let buttons = null
+    if (this.props.type === 'INVITE_TO_PROJECT') {
+      buttons = (
+        <div className="notif-buttons">
+          <Button onClick={this.acceptProject.bind(this)}>Accept</Button>
+          <Button onClick={this.declineProject.bind(this)}>Decline</Button>
+        </div>
+      );
+    }
+    let notifClassName = 'notif-item';
+    if (this.props.read) {
+      notifClassName += ' notif-read';
+    } else {
+      notifClassName += ' notif-unread';
     }
 
-    declineProject() {
-        let notifId = this.props.id
-        let projectId = this.props.meta.project_id
-        this.props.dispatch(declineProject(projectId, notifId))
+    let image = null;
+    if (this.props.user) {
+      image = (
+        <UserAvatar
+          imgSrc={this.props.user.display_image}
+          displayName={this.props.user.display_name}
+        />
+      );
     }
 
-    render() {
-        let buttons = null
-        if (this.props.type === 'INVITE_TO_PROJECT') {
-            buttons = (
-                <div className="notif-buttons">
-                    <Button onClick={this.acceptProject.bind(this)}>Accept</Button>
-                    <Button onClick={this.declineProject.bind(this)}>Decline</Button>
-                </div>
-            )
-        }
-        let notifClassName = 'notif-item'
-        if (this.props.read) {
-            notifClassName += ' notif-read'
-        } else {
-            notifClassName += ' notif-unread'
-        }
-
-        let image = null
-        if (this.props.user) {
-            image = (
-              <UserAvatar
-                imgSrc={this.props.user.display_image}
-                displayName={this.props.user.display_name}
-              />
-            );
-        }
-
-        return (
-            <li className={notifClassName}>
-                <div className="notif-photo">
-                    {image}
-                </div>
-                <div>
-                    <span className='notif-text'>{this.props.text}</span>
-                </div>
-                <span className='notif-fuzzy-time'>{this.props.fuzzyTime}</span>
-                {buttons}
-            </li>
-        );
-    }
+    return (
+      <li className={notifClassName}>
+        <div className="notif-photo">
+          {image}
+        </div>
+        <div>
+          <span className="notif-text">{this.props.text}</span>
+        </div>
+        <span className="notif-fuzzy-time">{this.props.fuzzyTime}</span>
+        {buttons}
+      </li>
+    );
+  }
 }
 export default NotificationItem;
--- assets/app/js/components/Notification/NotificationList.jsx
+++ assets/app/js/components/Notification/NotificationList.jsx
@@ -1,41 +1,44 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 
 import NotificationItem from './NotificationItem.jsx';
-import {toFuzzyTime} from '../../utils/general'
+import { toFuzzyTime } from '../../utils/general';
 
-class NotificationList extends Component {
-    render() {
-        let notifsSortedByTime = this.props.notifs.sort(function(a, b) {
-            return a.time < b.time
-        })
-        let notificationItems = notifsSortedByTime.map(notif => {
-            let matchingUsers = this.props.users.filter(user => user.id === notif.meta.user_id)
-            let user = null
-            if (matchingUsers.length >= 1) {
-                user = matchingUsers[0]
-            }
-
-            return <NotificationItem
-                id={notif.id}
-                key={notif.id}
-                text={notif.text}
-                type={notif.type}
-                meta={notif.meta}
-                read={notif.read}
-                user={user}
-                dispatch={this.props.dispatch}
-                fuzzyTime={toFuzzyTime(notif.time)} />
-
-            }
-        );
-
-        return (
-            <div className='notification-list'>
-                <ul>
-                    {notificationItems}
-                </ul>
-            </div>
-        );
+const propTypes = {
+  notifications: PropTypes.array.isRequired,
+  users: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+const NotificationList = ({ notifications, users, dispatch }) => {
+  const notifsSortedByTime = notifications.sort((a, b) => (a.time < b.time));
+  const notificationItems = notifsSortedByTime.map(notif => {
+    const matchingUsers = users.filter(user => user.id === notif.meta.user_id);
+    let user = null;
+    if (matchingUsers.length >= 1) {
+      user = matchingUsers[0];
     }
-}
+    return (
+      <NotificationItem
+        id={notif.id}
+        key={notif.id}
+        text={notif.text}
+        type={notif.type}
+        meta={notif.meta}
+        read={notif.read}
+        user={user}
+        dispatch={dispatch}
+        fuzzyTime={toFuzzyTime(notif.time)}
+      />
+    );
+  });
+
+  return (
+    <div className="notification-list">
+      <ul>
+        {notificationItems}
+      </ul>
+    </div>
+  );
+};
+
+NotificationList.propTypes = propTypes;
 export default NotificationList;
