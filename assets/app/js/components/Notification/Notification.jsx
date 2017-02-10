c078e9807badce34234ed328cb666719e1e64d38
Zhang Ji <zhangji951027@gmail.com>
2017-02-10 22:50:17 +0800

    resolve eslint warning on Notification components

--- assets/app/js/components/Notification/Notification.jsx
+++ assets/app/js/components/Notification/Notification.jsx
@@ -1,26 +1,24 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 import NotificationList from './NotificationList.jsx';
 
-class Notification extends Component {
-    render() {
-        const {notifications, dispatch, users} = this.props;
-        return (
-            <div className='main-content notif-container'>
-                <h4>All Notifications</h4>
-                <NotificationList
-                    notifs={notifications}
-                    dispatch={dispatch}
-                    users={users}
-                />
-            </div>
-        );
-    }
-}
-
-Notification.propTypes = {
-    notifications: PropTypes.array.isRequired,
-    users: PropTypes.array.isRequired
+const propTypes = {
+  notifications: PropTypes.array.isRequired,
+  users: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
+
+const Notification = ({ notifications, users, dispatch }) => (
+  <div className="main-content notif-container">
+    <h4>All Notifications</h4>
+    <NotificationList
+      notifications={notifications}
+      dispatch={dispatch}
+      users={users}
+    />
+  </div>
+);
+
+Notification.propTypes = propTypes;
 
 export default Notification;
--- assets/app/js/config/routes.jsx
+++ assets/app/js/config/routes.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import {Route, IndexRoute, IndexRedirect} from 'react-router'
-import App from '../containers/App.jsx'
-import Project from '../containers/Project.jsx'
-import _404 from '../components/_404.jsx'
-import NotificationView from '../containers/NotificationView.jsx'
-import SearchResults from '../components/SearchResults.jsx'
-import Dashboard from '../components/Dashboard.jsx'
+import React from 'react';
+import { Route, IndexRedirect } from 'react-router';
+import App from '../containers/App.jsx';
+import Project from '../containers/Project.jsx';
+import _404 from '../components/_404.jsx';
+import NotificationView from '../containers/NotificationView.jsx';
+import SearchResults from '../components/SearchResults.jsx';
+import Dashboard from '../components/Dashboard.jsx';
 import * as AppConstants from '../AppConstants';
 
-
 export default (
-  <Route path='/app' component={App}>
+  <Route path="/app" component={App}>
     <IndexRedirect to="dashboard" />>
-    <Route path='project/:id' component={Project}>
-      // <Route path={AppConstants.PATH.milestones} />
-      // <Route path={AppConstants.PATH.files} />
-      // <Route path={AppConstants.PATH.newsfeed} />
-      // <Route path={AppConstants.PATH.settings} />
+    <Route path="project/:id" component={Project}>
+      <Route path={AppConstants.PATH.milestones} />
+      <Route path={AppConstants.PATH.files} />
+      <Route path={AppConstants.PATH.newsfeed} />
+      <Route path={AppConstants.PATH.settings} />
     </Route>
-    <Route path='notifications' component={NotificationView}/>
-    <Route path='search' component={SearchResults}/>
-    <Route path='dashboard' component={Dashboard}/>
-    <Route path='*' component={_404}/>
-
+    <Route path="notifications" component={NotificationView} />
+    <Route path="search" component={SearchResults} />
+    <Route path="dashboard" component={Dashboard} />
+    <Route path="*" component={_404} />
   </Route>
 );
