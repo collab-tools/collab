afc439b6d0a411714920d61969237b831bd24098
Benjamin <benjamin.heng@thecarousell.com>
2017-02-12 23:03:03 +0800

    Add more tests for creating a task

--- tests/task.spec.js
+++ tests/task.spec.js
@@ -9,27 +9,39 @@ describe('Task', function() {
   beforeEach(function(done) {
     this.socketMock = sinon.mock(socket);
     this.githubMock = sinon.mock(github);
-    let user = null;
 
     models.User
-      .create({ id: 'user1' })
+      .create({
+        id: 'user1',
+        github_login: 'github_login1',
+      })
       .then((newUser) => {
-        user = newUser;
+        this.user = newUser;
         return models.Project.create({
           id: 'project1',
+          github_repo_owner: 'user1',
+          github_repo_name: 'repo_name',
         });
       })
-      .then((newProject) => user.addProject(newProject, { role: constants.ROLE_CREATOR }))
+      .then((newProject) => {
+        this.project = newProject;
+        return this.user.addProject(newProject, { role: constants.ROLE_CREATOR });
+      })
       .then(() => models.Milestone.create({
         id: 'milestone1',
         project_id: 'project1',
+        github_number: 1,
       }))
-      .then(() => models.Task.create({
-        id: 'task1',
-        project_id: 'project1',
-        milestone_id: 'milestone1',
-      }))
-      .then(() => {
+      .then((newMilestone) => {
+        this.milestone = newMilestone;
+        return models.Task.create({
+          id: 'task1',
+          project_id: 'project1',
+          milestone_id: 'milestone1',
+        });
+      })
+      .then((newTask) => {
+        this.task = newTask;
         done();
       });
   });
@@ -81,7 +93,6 @@ describe('Task', function() {
 
   describe('Create Task', function() {
     beforeEach(function(done) {
-      this.socketMock = sinon.mock(socket);
       this.payload = {
         content: 'task content',
         project_id: 'project1',
@@ -115,7 +126,7 @@ describe('Task', function() {
       });
     });
 
-    it('should create a task without milestone', function(done) {
+    it('should create a task without github milestone/assignee', function(done) {
       server.select('api').inject({
         method: 'POST',
         url: '/tasks',
@@ -130,6 +141,76 @@ describe('Task', function() {
         this.githubMock
           .expects('createGithubIssue')
           .once();
+        models.Task.findById('task1').then(createdTask => {
+          expect(createdTask).to.not.be.a('null');
+          done();
+        });
+      });
+    });
+
+    it('should create a task with github milestone', function(done) {
+      this.payload.milestone_id = 'milestone1';
+
+      server.select('api').inject({
+        method: 'POST',
+        url: '/tasks',
+        credentials: { user_id: 'user1', password: 'password1' },
+        payload: JSON.stringify(this.payload),
+      }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        this.socketMock
+          .expects('sendMessageToProject')
+          .once()
+          .withArgs(this.payload.project_id, 'new_task');
+        const issue = {
+          title: this.payload.content,
+          milestone: this.milestone.github_number,
+        };
+        this.githubMock
+          .expects('createGithubIssue')
+          .once()
+          .withArgs(
+            this.project.github_repo_owner,
+            this.project.github_repo_name,
+            this.payload.request_token,
+            issue
+          );
+        models.Task.findById('task1').then(createdTask => {
+          expect(createdTask).to.not.be.a('null');
+          done();
+        });
+      });
+    });
+
+    it('should create a task with github milestone and assignee', function(done) {
+      this.payload.milestone_id = 'milestone1';
+      this.payload.assignee_id = 'user1';
+
+      server.select('api').inject({
+        method: 'POST',
+        url: '/tasks',
+        credentials: { user_id: 'user1', password: 'password1' },
+        payload: JSON.stringify(this.payload),
+      }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        this.socketMock
+          .expects('sendMessageToProject')
+          .once()
+          .withArgs(this.payload.project_id, 'new_task');
+        const issue = {
+          title: this.payload.content,
+          milestone: this.milestone.github_number,
+          assignee: this.user.github_login,
+        };
+        this.githubMock
+          .expects('createGithubIssue')
+          .once()
+          .withArgs(
+            this.project.github_repo_owner,
+            this.project.github_repo_name,
+            this.payload.request_token,
+            issue
+          );
         models.Task.findById('task1').then(createdTask => {
           expect(createdTask).to.not.be.a('null');
           done();
