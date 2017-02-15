8aad32c70a3485c1ff53edf86058c2e12ab4dba5
Zhang Ji <zhangji951027@gmail.com>
2017-02-15 16:18:19 +0800

    resolve file path issue on assigneeRow.test

--- assets/app/test/components/AssigneeRow.test.js
+++ assets/app/test/components/AssigneeRow.test.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
-import AssigneeRow from './../../js/components/AssigneeRow.jsx';
+import AssigneeRow from './../../js/components/Milestone/AssigneeRow.jsx';
