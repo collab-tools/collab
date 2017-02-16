e00032fd992dff9f4988f7e5e25df986c6a36673
Zhang Ji <zhangji951027@gmail.com>
2017-02-16 17:20:06 +0800

    fix bugs on search

--- assets/app/js/UserColours.js
+++ assets/app/js/UserColours.js
@@ -1,12 +1,12 @@
 import { red500, pink500, purple500, deepPurple500, indigo500, blue500, lightBlue500,
   cyan500, teal500, green500, lightGreen500, lime500, yellow500, amber500, orange500,
-  deepOrange500, brown500, blueGrey500, grey500,
+  deepOrange500, brown500, blueGrey500, grey500, darkBlack,
 } from 'material-ui/styles/colors.js';
 
 const UserColours = [red500, pink500, purple500, deepPurple500,
   indigo500, blue500, lightBlue500, cyan500, teal500,
   green500, lightGreen500, lime500, yellow500, amber500,
-  orange500, deepOrange500, brown500, blueGrey500, grey500];
+  orange500, deepOrange500, brown500, blueGrey500, grey500, darkBlack];
 
 
 export default UserColours;
