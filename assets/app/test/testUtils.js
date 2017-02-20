fb6a74fb5c4143ea5aed9495e556c885258df6a0
Zhang Ji <zhangji951027@gmail.com>
2017-02-20 16:39:20 +0800

    add deep-freeze dependency and helper function for reducer testing

--- assets/app/test/testUtils.js
+++ assets/app/test/testUtils.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { expect } from 'chai'; // eslint-disable-line
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import deepFreeze from 'deep-freeze'; // eslint-disable-line
 
 const muiTheme = getMuiTheme();
 
-export const mountWithContext = (node) => {
-  return mount(node, {
-    context: {muiTheme},
-    childContextTypes: {muiTheme: React.PropTypes.object},
-  })
-};
+export const mountWithContext = (node) => (mount(node, {
+  context: { muiTheme },
+  childContextTypes: { muiTheme: React.PropTypes.object },
+}));
 
-export const shallowWithContext = (node) => {
-  return shallow(node, {
-    context: {muiTheme},
-    childContextTypes: {muiTheme: React.PropTypes.object},
+export const shallowWithContext = (node) => (
+  shallow(node, {
+    context: { muiTheme },
+    childContextTypes: { muiTheme: React.PropTypes.object },
   })
-};
+);
 
 export const debugWrapper = (wrapper) => {
-  return console.log(wrapper.debug());
-}
+  console.log(wrapper.debug()); // eslint-disable-line no-console
+};
+
+export const expectDeepEqual = (obj1, obj2) => {
+  expect(obj1).to.deep.equal(obj2);
+};
+
+export const expectReducerBehavior = (reducer, stateBefore, action, expected) => {
+  if (action) {
+    deepFreeze(action);
+  }
+  if (stateBefore) {
+    deepFreeze(stateBefore);
+  }
+  expectDeepEqual(reducer(stateBefore, action), expected);
+};
