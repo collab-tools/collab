19a69b725507845ec9e7d768fe6e08d40ef9d4bf
Zhang Ji <zhangji951027@gmail.com>
2017-02-20 16:44:02 +0800

    add reducer test cases for search and snackbar

--- assets/app/js/reducers/snackbar.js
+++ assets/app/js/reducers/snackbar.js
@@ -1,24 +1,31 @@
 import assign from 'object-assign';
 import * as AppConstants from '../AppConstants';
+/*
+Example state tree:
+const snackbar = {
+  isOpen: false,
+  message: '',
+  background: '',
+}
+*/
 
-// Example state tree:
-// {
-// isOpen(pin):false,
-// message:"",
-// background:""
-// }
+const colorDefault = 'rgba(0, 0, 0, 0.870588)';
+const colorWaring = 'rgba(226, 88, 88, 0.870588)';
+const colorInfo = 'rgba(57, 144, 208, 0.870588)';
+
 const snackbar = (state = {}, action) => {
   switch (action.type) {
     case AppConstants.INIT_SNACKBAR:
       return action.snackbar;
-    case AppConstants.SNACKBAR_MESSAGE:
-      let colour = 'rgba(0, 0, 0, 0.870588)';
+    case AppConstants.SNACKBAR_MESSAGE: {
+      let colour = colorDefault;
       if (action.kind === 'warning') {
-        colour = 'rgba(226, 88, 88, 0.870588)';
+        colour = colorWaring;
       } else if (action.kind === 'info') {
-        colour = 'rgba(57, 144, 208, 0.870588)';
+        colour = colorInfo;
       }
       return assign({}, state, { isOpen: true, message: action.message, background: colour });
+    }
     case AppConstants.UPDATE_SNACKBAR:
       return assign({}, state, action.snackbar);
     default:
