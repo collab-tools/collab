d80392e106655dc2b3b397b993629721f48f885d
Zhang Ji <zhangji951027@gmail.com>
2017-03-01 17:53:43 +0800

    add skleton code for message components

---
+++ assets/app/js/components/Message/SystemMessage.jsx
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+
+import UserAvatar from '../Common/UserAvatar.jsx';
+import { toFuzzyTime } from '../../utils/general';
+
+
+const propTypes = {
+  message: PropTypes.object.isRequired,
+};
+
+const SystemMssage = ({ message }) => (
+  <li className="event-item">
+    <div className="notif-photo">
+      <UserAvatar
+        displayName={'system'}
+      />
+    </div>
+    <div>
+      <span className="notif-text">{message.content}</span>
+    </div>
+    <span className="notif-fuzzy-time">{toFuzzyTime(message.created_at)}</span>
+  </li>
+);
+
+SystemMssage.propTypes = propTypes;
+export default SystemMssage;
