e034f84b91876cbf5cc72b710673c3e3d775ef4f
Zhang Ji <zhangji951027@gmail.com>
2017-03-04 18:10:30 +0800

    refactor leftpane-related component; allow left pane to be toggled

---
+++ assets/app/js/components/ProjectListItem.jsx
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+import assign from 'object-assign';
+import { ListItem } from 'material-ui/List';
+import { Color } from '../myTheme.js';
+import ClippedText from './Common/ClippedText.jsx';
+
+const propTypes = {
+  projectId: PropTypes.string.isRequired,
+  projectContent: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+  onSwitchProject: PropTypes.func.isRequired,
+  itemStyle: PropTypes.object,
+};
+const NUMBER_LIMIT = 20;
+const ProjectListItem = ({ projectId, projectContent, active, onSwitchProject, itemStyle }) => {
+  const switchProject = onSwitchProject.bind(null, projectId);
+  return (
+    <ListItem
+      key={projectId}
+      onTouchTap={switchProject}
+      innerDivStyle={assign({}, itemStyle, active && {
+        backgroundColor: Color.leftPanelItemHightColor,
+      })}
+      hoverColor={Color.leftPanelItemHightColor}
+      primaryText={<ClippedText text={projectContent} limit={NUMBER_LIMIT} />}
+    />
+  );
+};
+ProjectListItem.propTypes = propTypes;
+
+export default ProjectListItem;
