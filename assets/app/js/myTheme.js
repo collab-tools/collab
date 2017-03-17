583475ba64a0c2f502d01323c421e8d65c80500e
Zhang Ji <zhangji951027@gmail.com>
2017-03-18 04:35:04 +0800

    integrate material-ui theme

--- assets/app/js/myTheme.js
+++ assets/app/js/myTheme.js
@@ -1,30 +1,29 @@
-import { cyan500, cyan700, white, pink200,
-  grey100, grey300, grey500, blueGrey900, black } from 'material-ui/styles/colors.js';
+import { cyan500, cyan700, white, pinkA200, grey100, grey300, grey400, grey500, blueGrey900, black, darkBlack, fullBlack } from 'material-ui/styles/colors.js';
 import { fade } from 'material-ui/utils/colorManipulator.js';
-import Spacing from 'material-ui/styles/spacing';
+import spacing from 'material-ui/styles/spacing';
 import zIndex from 'material-ui/styles/zIndex';
 
-const MyRawTheme = {
-  spacing: Spacing,
-  zIndex,
+export default {
+  spacing,
   fontFamily: 'Roboto, sans-serif',
   palette: {
     primary1Color: cyan500,
     primary2Color: cyan700,
-    primary3Color: white,
-    accent1Color: pink200,
+    primary3Color: grey400,
+    accent1Color: pinkA200,
     accent2Color: grey100,
     accent3Color: grey500,
-    textColor: grey100,
-    alternateTextColor: grey100,
-    canvasColor: blueGrey900,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
     borderColor: grey300,
-    disabledColor: fade(white, 0.3),
+    disabledColor: fade(darkBlack, 0.3),
     pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
   },
 };
 
-export default MyRawTheme;
 export const Color = {
   leftPanelItemHightColor: fade(black, 0.4),
   leftPanelBackgroundColor: '#37474f',
