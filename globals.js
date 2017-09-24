b28d167959fe69c88ace29cb8182694058aacbca
Benjamin <benjamin.heng@thecarousell.com>
2017-09-24 14:01:44 +0800

    tmp commit

---
+++ globals.js
@@ -0,0 +1,15 @@
+var chromedriver = require('chromedriver');
+var binPath = chromedriver.path;
+module.exports = {
+  before : function(done) {
+    console.log('starting chromedriver');
+    chromedriver.start();
+    done();
+  },
+
+  after : function(done) {
+    console.log('stopping chromedriver');
+    chromedriver.stop();
+    done();
+  }
+};
---
+++ nightwatch.conf.js
@@ -0,0 +1,36 @@
+module.exports = {
+  src_folders: ['assets/app/teste2e/'],
+  output_folder: 'reports',
+  custom_commands_path: '',
+  custom_assertions_path: '',
+  page_objects_path: '',
+  globals_path: './globals.js',
+
+  selenium: {
+    start_process: false,
+  },
+
+  test_settings: {
+    default: {
+      default_path_prefix: '',
+      selenium_port: 9515,
+      selenium_host: '127.0.0.1',
+      launch_url: 'http://localhost',
+      javascriptEnabled: true,
+      acceptSslCerts: true,
+      trustAllSSLCerficates: true,
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          binary: './node_modules/puppeteer/.local-chromium/linux-499413/chrome-linux/chrome',
+          args: [
+            '--no-sandbox',
+            '--headless',
+            'start-fullscreen',
+            'window-size=1280,800',
+          ],
+        },
+      },
+    },
+  },
+};
