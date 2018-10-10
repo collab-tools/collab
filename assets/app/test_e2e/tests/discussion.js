b4164b1417bad67dfda29b8f7fd997516c3d931f
AHEADer <dqawveird@gmail.com>
2018-10-10 20:41:38 +0800

    enlarge time to pass in CI server

--- assets/app/test_e2e/tests/discussion.js
+++ assets/app/test_e2e/tests/discussion.js
@@ -30,22 +30,25 @@ module.exports = {
     const homepage = client.page.app();
     homepage.waitForElementPresent(`#project-${projectName}`, 5000);
     homepage.click(`#project-${projectName}`).api.pause(1000);
-    //homepage.waitForElementVisible('button[class=]', 1000);
     homepage.click('@discussionsTab');
     homepage.waitForElementVisible('@discussionsContainer', 2000);
 
     // Expand message options, click "edit" button
     homepage.click('.message-list .message-row:last-child .message-options-btn');
+    homepage.api.pause(2000);
     homepage.waitForElementVisible('@editMessageBtn', 2000);
-    homepage.click('@editMessageBtn').api.pause(1000);
+    homepage.api.pause(2000);
+    homepage.click('@editMessageBtn');
+    homepage.api.pause(2000);
 
     // Replace message content and submit
     homepage.waitForElementVisible('@addMessageInput', 2000);
+    homepage.api.pause(2000);
     homepage.clearValue('@addMessageInput');
-    homepage.api.pause(500);
+    homepage.api.pause(2000);
     homepage.setValue('@addMessageInput', editedMessageText);
+    homepage.api.pause(2000);
     homepage.click('@addMessageSubmitBtn').api.pause(1000);
-    
     homepage.expect.element('.message-list .message-row:last-child .message-content p')
       .text.to.equal(editedMessageText);
 
@@ -58,21 +61,26 @@ module.exports = {
     // Edits the last message
     const homepage = client.page.app();
     homepage.waitForElementPresent(`#project-${projectName}`, 5000);
-    homepage.click(`#project-${projectName}`).api.pause(500);
-    homepage.api.pause(500)
+    homepage.click(`#project-${projectName}`).api.pause(1000);
+    homepage.api.pause(2000);
     homepage.click('@discussionsTab');
+    homepage.api.pause(2000);
     homepage.waitForElementVisible('@discussionsContainer', 2000);
+    homepage.api.pause(2000);
     homepage.expect.element('@pinnedMessage').to.not.be.present;
+    homepage.api.pause(2000);
 
     // Expand message options, click "pin" button
     homepage.click('.message-list .message-row:last-child .message-options-btn');
+    homepage.api.pause(2000);
     homepage.waitForElementVisible('@pinMessageBtn', 2000);
+    homepage.api.pause(2000);
     homepage.click('@pinMessageBtn');
+    homepage.api.pause(2000);
     homepage.waitForElementVisible('@pinnedMessage', 2000);
 
     // Snackbar displays "Message pinned"
     homepage.waitForElementVisible('@systemSnackbar', 2000);
-    homepage.api.pause(500);
     homepage.expect.element('@systemSnackbar').text.to.equal('Message pinned');
 
     // Wait for snackbar message to close
@@ -80,13 +88,16 @@ module.exports = {
 
     // Expand message options, click "unpin" button
     homepage.click('.message-list .message-row:last-child .message-options-btn');
+    homepage.api.pause(2000);
     homepage.waitForElementVisible('@unpinMessageBtn', 2000);
+    homepage.api.pause(2000);
     homepage.click('@unpinMessageBtn');
 
     // Snackbar displays "Message unpinned"
     homepage.waitForElementVisible('@systemSnackbar', 2000);
+    homepage.api.pause(2000);
     homepage.expect.element('@systemSnackbar').text.to.equal('Message unpinned');
-
+    homepage.api.pause(2000);
     // Assert that there are no more pinned messages
     homepage.api.pause(500);
     homepage.expect.element('@pinnedMessage').to.not.be.present;
